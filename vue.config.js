2252c3932f1411e6e8290d284049a56a714df12a
S <s@Ss-MacBook-Pro.local>
2024-11-05 21:04:04 +0300

    Prepare backend for deployment

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+// vue.config.js
+const webpack = require('webpack');
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+      }),
+    ],
+  },
+};
